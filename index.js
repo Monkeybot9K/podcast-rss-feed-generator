4b001e822753807b27454e8813b5fd613bb59402
Santiago M Vilar <santiagov@mural.ly>
2016-08-10 23:40:33 -0300

    Fetch items from archive collection and return with template

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const config = require('./config');
+const podcast = require('./lib/podcast');
+
+const app = express();
+
+app.get('/',
+  podcast.fetchFeed,
+  (req, res) => {
+    res.type('xml');
+    res.status(200).send(req.feed);
+  }
+);
+
+app.listen(config.port, () => {
+  console.log(`Running on port ${config.port}`);
+});
